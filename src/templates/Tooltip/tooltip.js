dace216c8240fe9b5f5a6f1872536b5c2641f7d9
ZiZIGY <zizigydev@gmail.com>
2025-09-12 03:28:47 +0300

    added disabled for tooltip

--- src/templates/Tooltip/tooltip.js
+++ src/templates/Tooltip/tooltip.js
@@ -1,30 +1,19 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
-    
+
     this._tooltip = null;
     this._trigger = null;
-    
+
     this.attachShadow({ mode: 'open' });
   }
 
   connectedCallback() {
     this._render();
-    this._updateAriaAttributes();
   }
 
   disconnectedCallback() {
     this._cleanup();
-  }
-
-  static get observedAttributes() {
-    return [, 'disabled'];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'disabled') {
-      this._updateAriaAttributes();
-    }
   }
 
   _render() {
@@ -38,17 +27,9 @@ class Tooltip extends HTMLElement {
         <slot name="content"></slot>
       </div>
     `;
-  
+
     this._tooltip = this.shadowRoot.querySelector('.tooltip');
     this._trigger = this.shadowRoot.querySelector('slot[name="trigger"]');
-  }
-
-  _updateAriaAttributes() {
-    if (this.hasAttribute('disabled')) {
-      this.removeAttribute('aria-describedby');
-    } else {
-      this.setAttribute('aria-describedby', 'tooltip-content');
-    }
   }
 }
 
