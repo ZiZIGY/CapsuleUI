1df466b2f5f5be710d8c1bd2b5bf21875f7e5609
ZiZIGY <zizigydev@gmail.com>
2025-09-23 18:25:52 +0300

    change slider variables

--- src/templates/Slider/slider.js
+++ src/templates/Slider/slider.js
@@ -13,7 +13,6 @@ class Slider extends HTMLElement {
 
   constructor() {
     super();
-    this._render();
     this.attachShadow({ mode: 'open' });
     this.internals_ = this.attachInternals();
     this._min = 0;
@@ -37,6 +36,7 @@ class Slider extends HTMLElement {
 
   connectedCallback() {
     this._parseAttributes();
+    this._render();
     this._init();
   }
 
