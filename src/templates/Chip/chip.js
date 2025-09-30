b82c7cb4691f089fbc1d84e3e2ab7dc67738ef0a
ZiZIGY <zizigydev@gmail.com>
2025-09-30 03:12:04 +0300

    adaprive chip

--- src/templates/Chip/chip.js
+++ src/templates/Chip/chip.js
@@ -1,18 +1,6 @@
 class Chip extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
-  }
-
-  connectedCallback() {
-    this._render();
-  }
-
-  _render() {
-    this.shadowRoot.innerHTML = `
-      <slot></slot>
-      <capsule-ripple></capsule-ripple>
-    `;
   }
 }
 
